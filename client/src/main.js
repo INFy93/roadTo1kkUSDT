e13c2e60c902496eeeb64cc8ab32db3c135d78b1
INFy93 <infy93@gmail.com>
2024-07-15 21:59:41 +0300

    add highcharts to app.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import HighchartsVue from 'highcharts-vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 
 app.use(router)
+app.use(HighchartsVue)
 
 app.mount('#app')
